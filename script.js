a30d8ad49273539262f8a2ed0840c470a61709ff
Sarah_Mahmoud21 <saramahmoud15420@gmail.com>
2024-03-06 10:02:16 +0200

    commit

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    generatePics();
+});
+
 function toggleMenu() {
     let content = document.getElementsByClassName('boxes')[0];
     let button = document.getElementById('filters');
@@ -11,4 +15,31 @@ function toggleMenu() {
         content.style.display = 'none';
         button.innerHTML = '<i class="fas fa-bars"></i>' + ' Show filters';
     }
+}
+function generatePics() {
+    let picturesContainer = document.getElementsByClassName('pictures')[0];
+
+    for (let i = 0; i < 15; i++) {
+        let figure = document.createElement('figure');
+
+        let img = document.createElement('img');
+        img.src = 'https://i.pinimg.com/564x/fe/ca/c0/fecac007779f56c19a9892dedefa2ba0.jpg';
+        img.alt = 'dress';
+
+        let figcaption = document.createElement('figcaption');
+        figcaption.textContent = 'Pink Short Dress';
+
+        let button = document.createElement('button');
+        button.textContent = 'Add to Cart';
+        
+    
+
+        figure.appendChild(img);
+        figure.appendChild(figcaption);
+        figure.appendChild(button);
+
+        // Append the figure to the pictures container
+        picturesContainer.appendChild(figure);
+      
+}
 }
